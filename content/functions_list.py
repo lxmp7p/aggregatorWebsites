4763edb610228854ff5dc5a4e29fb47fc48e7497
BuildTools <unconfigured@null.spigotmc.org>
2020-10-15 19:41:47 +0300

    add xaker privacy page

--- content/functions_list.py
+++ content/functions_list.py
@@ -22,18 +22,44 @@ def get_news_from_site(soup, url):
     elif 'codeby.net' in url:
         news = soup.findAll("h2", {"class": "block-header"})
         get_codeby_news(news, url)
+    elif 'xakep.ru/category/privacy/' in url:
+        news = soup.findAll("h3", {"class": "entry-title"})
+        get_xaker_news(news, url)
 
 def get_habr_news(news, link):
-    list_news = []
+    count = 0
     for i in news:
+        if count == 5:
+            break
+        count += 1
         name = check_name_len(i.text)
         bd = newsList(site = link,
                       url = i.get_attribute_list('href')[0],
                       header = name,)
         bd.save()
 
-def get_codeby_news(news, link):
+def get_xaker_news(news, link):
+    count = 0
     for i in news:
+        if count == 5:
+            break
+        count += 1
+        # print(i)
+        temp = str(i.get_attribute_list)
+        url = str(re.findall('https.*\/"', temp))[2:-3]
+        header = str(re.findall('<span>.*<\/s', temp))[8:-5]
+        name = check_name_len(header)
+        bd = newsList(site = link,
+                      url = url,
+                      header = name,)
+        bd.save()
+
+def get_codeby_news(news, link):
+    count = 0
+    for i in news:
+        if count == 5:
+            break
+        count += 1
         temp = str(i.get_attribute_list)
         temp = temp.replace('>', ';')
         tmp = []
--- content/views.py
+++ content/views.py
@@ -9,12 +9,18 @@ def watch_content(request):
     newsList.objects.all().delete()
     url_list = ['https://habr.com/ru/hub/python/',
                 'https://codeby.net/',
-                'https://habr.com/ru/top/',]
+                'https://habr.com/ru/top/',
+                'https://xakep.ru/category/privacy/',]
     for site in url_list:
         get_news(request, site)
 
-    codebyList = newsList.objects.filter(site='https://codeby.net/')[:5]
-    habrTopList = newsList.objects.filter(site='https://habr.com/ru/top/')[:5]
-    habrPyList = newsList.objects.filter(site='https://habr.com/ru/hub/python/')[:5]
-    return render(request, 'watchContentPage.html', {'codebyList': codebyList, 'habrTopList':habrTopList, 'habrPyList':habrPyList })
+    codebyList = newsList.objects.filter(site='https://codeby.net/')
+    habrTopList = newsList.objects.filter(site='https://habr.com/ru/top/')
+    habrPyList = newsList.objects.filter(site='https://habr.com/ru/hub/python/')
+    xakerPrivacyList = newsList.objects.filter(site='https://xakep.ru/category/privacy/')
+    return render(request, 'watchContentPage.html', {'codebyList': codebyList,
+                                                     'habrTopList':habrTopList,
+                                                     'habrPyList':habrPyList,
+                                                     'xakerPrivacyList':xakerPrivacyList,
+                                                    })
 
