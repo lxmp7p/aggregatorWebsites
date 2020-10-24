9cb87e7ac6aeda1908591172efd69c44ecfa4d56
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 18:55:43 +0300

    add options page

--- content/urls.py
+++ content/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', watch_content, name="watch_content"),
+    path('options/', open_options, name="options"),
 ]
--- content/views.py
+++ content/views.py
@@ -5,22 +5,38 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .functions_list import *
 from .models import newsList
+
+sites = ['HabrPython','HabrTop','Codeby','Xakep',]
+
+habr_python = { 'name':'Хабр питон', 'url': 'https://habr.com/ru/hub/python/', }
+habr_top = { 'name':'Хабр топ', 'url': 'https://habr.com/ru/top/', }
+codeby = { 'name':'Codeby', 'url': 'https://codeby.net/', }
+xakep = { 'name':'Xakep', 'url': 'https://xakep.ru/category/privacy/', }
+
 def watch_content(request):
     newsList.objects.all().delete()
-    url_list = ['https://habr.com/ru/hub/python/',
-                'https://codeby.net/',
-                'https://habr.com/ru/top/',
-                'https://xakep.ru/category/privacy/',]
+    url_list = [habr_python.get('url'),
+                habr_top.get('url'),
+                codeby.get('url'),
+                xakep.get('url'),]
     for site in url_list:
         get_news(request, site)
 
-    codebyList = newsList.objects.filter(site='https://codeby.net/')
-    habrTopList = newsList.objects.filter(site='https://habr.com/ru/top/')
-    habrPyList = newsList.objects.filter(site='https://habr.com/ru/hub/python/')
-    xakerPrivacyList = newsList.objects.filter(site='https://xakep.ru/category/privacy/')
+    codebyList = newsList.objects.filter(site=codeby.get('url'))
+    habrTopList = newsList.objects.filter(site=habr_top.get('url'))
+    habrPyList = newsList.objects.filter(site=habr_python.get('url'))
+    xakepPrivacyList = newsList.objects.filter(site=xakep.get('url'))
     return render(request, 'watchContentPage.html', {'codebyList': codebyList,
                                                      'habrTopList':habrTopList,
                                                      'habrPyList':habrPyList,
-                                                     'xakerPrivacyList':xakerPrivacyList,
+                                                     'xakerPrivacyList':xakepPrivacyList,
                                                     })
 
+def open_options(request):
+    print(request.POST)
+    context = {'habr_python':'eqwqwwqe',
+               'habr_top':habr_top,
+               'codeby':codeby,
+               'xakep':xakep,}
+
+    return render(request, 'options.html', {'sites':sites})
